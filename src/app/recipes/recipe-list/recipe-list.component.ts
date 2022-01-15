d5da5381609ced9653e33a6ec3e7a2294d4a4797
rickywychoi <cchaff0105@gmail.com>
2022-01-15 14:14:11 -0800

    End of basic ui for recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe(
+      'KFC',
+      'A fried chicken',
+      'https://singaporelocalfavourites.com/wp-content/uploads/2018/05/hot-and-spicy-chicken-e1598019670219.jpg'
+    ),
+    new Recipe(
+      'KFC',
+      'A fried chicken',
+      'https://singaporelocalfavourites.com/wp-content/uploads/2018/05/hot-and-spicy-chicken-e1598019670219.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public onClickNewRecipe() {
+    console.log('New recipe added');
   }
-
 }
